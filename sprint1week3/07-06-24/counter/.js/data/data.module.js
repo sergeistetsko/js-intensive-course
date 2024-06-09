b6dbf0ebd0be521784f15e89e118967fcf9df975
sergeistetsko <sergeistetsko2021@yandex.by>
2024-06-09 10:00:50 +0300

    add step 4

--- sprint1week3/07-06-24/counter/.js/data/data.module.js
+++ sprint1week3/07-06-24/counter/.js/data/data.module.js
@@ -1,4 +1,15 @@
+import { render } from '../render/render.module.js'
+
 export const data = {
     maxValue: 10,
     currentValue: 3
 }
+
+function increment() {
+    if (data.currentValue < data.maxValue) {
+        data.currentValue = data.currentValue + 1
+    }
+    render(data)
+}
+
+setInterval(increment, 1000)
--- sprint1week3/07-06-24/counter/.js/render/render.module.js
+++ sprint1week3/07-06-24/counter/.js/render/render.module.js
@@ -2,6 +2,7 @@ import { Counter } from './components/Counter/Counter.js'
 
 export function render(data) {
     const root = document.getElementById('root')
+    root.innerHTML = ''
 
     root.append(
         Counter(data)
