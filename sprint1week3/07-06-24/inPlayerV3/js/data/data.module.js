2cd2badc9a22b063f16c2989e9823f118a2b830b
sergeistetsko <sergeistetsko2021@yandex.by>
2024-06-10 22:43:14 +0300

    add changes

--- sprint1week3/07-06-24/inPlayerV3/js/data/data.module.js
+++ sprint1week3/07-06-24/inPlayerV3/js/data/data.module.js
@@ -21,8 +21,24 @@ export const data = {
                     artistName: '50 cent',
                     trackTitle: 'In da Club',
                     isHot: false,
-                    trackFileUrl: './audio/50 cent - In da Club.mp3',
+                    trackFileUrl: './audio/50cent - In da club.mp3',
                     trackCoverImgUrl: './img/cardImage/trackList/track2.jpeg',
+                },
+                {
+                    trackId: '3',
+                    artistName: 'DMX',
+                    trackTitle: 'X Gon\' Give It To Ya',
+                    isHot: false,
+                    trackFileUrl: './audio/DMX - X Gon\' Give It To Ya.mp3',
+                    trackCoverImgUrl: './img/cardImage/trackList/track3.jpeg',
+                },
+                {
+                    trackId: '4',
+                    artistName: 'Eminem',
+                    trackTitle: 'You Don\'t Know (feat. 50 cent, Lloyd Banks and Cashis)',
+                    isHot: false,
+                    trackFileUrl: './audio/Eminem - You Don\'t Know (feat. 50 cent, Lloyd Banks and Cashis).mp3',
+                    trackCoverImgUrl: './img/cardImage/trackList/track4.jpeg',
                 }
             ]
         },
@@ -47,8 +63,24 @@ export const data = {
                     artistName: 'Vanila Ice',
                     trackTitle: 'Ice Ice Baby',
                     isHot: false,
-                    trackFileUrl: './audio/Vanila Ice - Ice Ice Baby.mp3',
+                    trackFileUrl: './audio/Vanila Ice - Ice Baby.mp3',
                     trackCoverImgUrl: './img/cardImage/trackList/track6.jpeg',
+                },
+                {
+                    trackId: '7',
+                    artistName: 'Mc Hammer',
+                    trackTitle: 'You Can\'t Touch This',
+                    isHot: true,
+                    trackFileUrl: './audio/Mc Hammer - You Can\'t Touch This.mp3',
+                    trackCoverImgUrl: './img/cardImage/trackList/track7.jpeg',
+                },
+                {
+                    trackId: '8',
+                    artistName: 'Brand Nubian',
+                    trackTitle: 'Brand Nubian',
+                    isHot: false,
+                    trackFileUrl: './audio/Brand Nubian - Brand Nubian.mp3',
+                    trackCoverImgUrl: './img/cardImage/trackList/track8.jpeg',
                 }
             ]
         }
